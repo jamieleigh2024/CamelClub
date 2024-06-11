4ba9491fe88658c3ca1a0301645c2dc8ce304190
JamieLeigh <jamie-leigh-watson@hotmail.com>
2024-06-11 20:55:20 +0100

    Bottle class removed. I'm a little stuck with the staff class and how to create an instance of it with its parameters, in another class

--- Lesson 6.py
+++ Lesson 6.py
@@ -3,23 +3,25 @@ class Camel:
     This class represents Camel Club, Huddersfield, UK
     """
 
-    def __init__(self):
+    def __init__(self, staff_name):
         self.guests = 0
         self.money_gbp = 10000
         self.entrance_fee = 6
-        self.vk_bottles = VK()
-        self.champagne_bottles = Champagne()
+        self.vk_bottle = VK()
+        self.champagne_bottle = Champagne()
+        self.staff = Staff(name=staff_name)
 
     def sell_drink(self, drink_type):
         if isinstance(drink_type, VK):
-            self.vk_bottles.sell_vk()
+            self.vk_bottle.sell_vk()
+            self.staff.takings += self.vk_bottle.price
         if isinstance(drink_type, Champagne):
-            self.champagne_bottles.sell_champagne()
+            self.champagne_bottle.sell_champagne()
+            self.staff.takings += self.champagne_bottle.price
 
     def guest_arrives(self, age):
         if age < 18:
             return print("Guest not permitted due to age")
-
         elif age >= 18:
             self.money_gbp += self.entrance_fee
             self.guests += 1
@@ -28,24 +30,14 @@ class Camel:
         self.guests -= 1
 
 
-class Bottle:
-    """
-    This class represents all the bottles at Camel Club.
-    """
-    def __init__(self):
-        self.vk_bottles = 1200
-        self.champagne_bottles = 1200
-        self.min_vk_bottles = 1200
-        self.min_champagne_bottles = 1200
-
-
-class VK(Bottle):
+class VK:
 
     def __init__(self):
-        super().__init__()
         self.type = "VK"
         self.cost = 1
         self.price = 4
+        self.vk_bottles = 1200
+        self.min_vk_bottles = 1200
 
     def order_vk(self):
         vk_amount = self.min_vk_bottles - self.vk_bottles
@@ -55,13 +47,14 @@ class VK(Bottle):
         self.vk_bottles -= 1
 
 
-class Champagne(Bottle):
+class Champagne:
 
     def __init__(self):
-        super().__init__()
         self.type = "Champagne"
         self.cost = 25
         self.price = 60
+        self.champagne_bottles = 1200
+        self.min_champagne_bottles = 1200
 
     def order_champagne(self):
         champagne_amount = self.min_champagne_bottles - self.champagne_bottles
@@ -69,3 +62,10 @@ class Champagne(Bottle):
 
     def sell_champagne(self):
         self.champagne_bottles -= 1
+
+
+class Staff:
+
+    def __init__(self, name):
+        self.name = name
+        self.takings = 0
